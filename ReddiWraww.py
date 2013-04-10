c2037ca5f910bccc4bd0a757e7970883246b4f30
Eugenia Gabrielova <genia.likes.science@gmail.com>
2013-04-10 16:18:19 -0700

    Added datetime and upvote/downvote print statement from reddiwrap

--- ReddiWraww.py
+++ ReddiWraww.py
@@ -1,12 +1,26 @@
 # To print titles of top 10 in r/aww
 import urllib
 from ReddiWrap import ReddiWrap
+from datetime import datetime
+
+def getUTCTime(timestamp_string):
+    return datetime.utcfromtimestamp(timestamp_string)
+
+def getTime(timestamp_string):
+    return datetime.fromtimestamp(timestamp_string)
 
 # Create new ReddiWrap instance
 reddit = ReddiWrap()
 aww = reddit.get('/r/aww')
+strFormat = "%Y-%m-%d"
 
 topten = aww[:10]
+
+print "Title", "|", "Time", "|", "Votes + -"
 for p in topten:
-    image = urllib.urlopen("http://site.com/image.jpg").read()
-    print p.url, p.title
+    #image = urllib.urlopen("http://site.com/image.jpg").read()
+    #print p.url, p.title
+    print "=", p.title, "="
+    print "Votes | +:", p.upvotes, "-:", p.downvotes, "fuzzy:", p.score
+    print "Time[Local]:", getTime(p.created) ,"Time[UTC]:", getUTCTime(p.created_utc)
+    print
